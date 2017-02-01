9ec7b4d3990185bd5190fb7ac028a91d90eac8df
Richard Dally <r.dally@protonmail.com>
2017-02-01 19:04:53 +0100

    Add get_opposite_way method to Way class

--- way.py
+++ way.py
@@ -1,3 +1,10 @@
 class Way:
     BUY = 0
     SELL = 1
+
+    def get_opposite_way(self, way):
+        if way == Way.BUY:
+            return Way.SELL
+        elif way == Way.SELL:
+            return Way.BUY
+        raise Exception('Way is invalid')
