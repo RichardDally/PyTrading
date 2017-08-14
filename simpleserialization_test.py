88e30bada337c6f2a74656304cb5bd8b3360ab17
Richard Dally <r.dally@protonmail.com>
2017-08-15 00:31:01 +0200

    Improve simpleserialization_test.py

--- simpleserialization_test.py
+++ simpleserialization_test.py
@@ -15,8 +15,9 @@ class TestSimpleSerialization(unittest.TestCase):
     def test_empty_referential(self):
         empty_referential = Referential()
         encoded_referential = SimpleSerialization.encode_referential(empty_referential)
-        message_type, body, new_offset = SimpleSerialization.decode_header(encoded_referential)
+        message_type, body, _ = SimpleSerialization.decode_header(encoded_referential)
         decoded_referential = SimpleSerialization.decode_referential(body)
+        self.assertEqual(message_type, MessageTypes.Referential)
         self.assertEqual(empty_referential.__dict__, decoded_referential.__dict__)
 
     def test_default_referential(self):
@@ -30,7 +31,7 @@ class TestSimpleSerialization(unittest.TestCase):
     def test_empty_order_book(self):
         empty_order_book = OrderBook(StaticData.get_instrument(0).identifier)
         encoded_order_book = SimpleSerialization.encode_order_book(empty_order_book)
-        message_type, body, new_offset = SimpleSerialization.decode_header(encoded_order_book)
+        message_type, body, _ = SimpleSerialization.decode_header(encoded_order_book)
         decoded_order_book = SimpleSerialization.decode_order_book(body)
         self.assertEqual(message_type, MessageTypes.OrderBook)
         self.assertEqual(empty_order_book.__dict__, decoded_order_book.__dict__)
@@ -40,7 +41,7 @@ class TestSimpleSerialization(unittest.TestCase):
         buy_order = Order(Way.BUY, self.instrument.identifier, 100.0, 10.0, 'Trader1')
         simple_order_book.add_order(buy_order)
         encoded_order_book = SimpleSerialization.encode_order_book(simple_order_book)
-        message_type, body, new_offset = SimpleSerialization.decode_header(encoded_order_book)
+        message_type, body, _ = SimpleSerialization.decode_header(encoded_order_book)
         decoded_order_book = SimpleSerialization.decode_order_book(body)
         self.assertEqual(message_type, MessageTypes.OrderBook)
         self.assertEqual(encoded_order_book, SimpleSerialization.encode_order_book(decoded_order_book))
